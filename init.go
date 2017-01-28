5d8dffc0735c7e04540fb6275e15945ff059d2f1
Alexander Andryashin <aandryashin@gmail.com>
2017-01-28 18:22:18 +0300

    Initial commit

---
+++ init.go
@@ -0,0 +1,60 @@
+// Micro init process to run zero downtime servers within containers
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"os/signal"
+	"syscall"
+)
+
+func init() {
+	flag.Usage = func() {
+		fmt.Println("Usage: init COMMAND [args...]")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		flag.Usage()
+		os.Exit(1)
+	}
+	cmd := exec.Command(os.Args[1], os.Args[2:]...)
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	child, err := syscall.Getpgid(cmd.Process.Pid)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go io.Copy(os.Stdout, stdout)
+	go io.Copy(os.Stderr, stderr)
+
+	sig := make(chan os.Signal)
+	signal.Notify(sig)
+	signal.Ignore(syscall.SIGCHLD)
+	go func() {
+		for {
+			syscall.Kill(-(child), (<-sig).(syscall.Signal))
+		}
+	}()
+	var status syscall.WaitStatus
+	syscall.Wait4(-1, &status, 0, nil)
+}
