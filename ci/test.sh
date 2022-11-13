118edfb9e2cc6a96da79cf59e6d7c4996ef872fc
Ivan Krutov <vania-pooh@aerokube.com>
2022-11-13 08:51:04 +0300

    Bump Golang version to 1.19.3

--- ci/test.sh
+++ ci/test.sh
@@ -2,3 +2,6 @@
 
 export GO111MODULE="on"
 go test -v -race ./...
+
+go install golang.org/x/vuln/cmd/govulncheck@latest
+"$(go env GOPATH)"/bin/govulncheck ./...
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/aerokube/init
 
-go 1.17
+go 1.19
